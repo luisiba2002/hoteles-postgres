5582f5f43ce283ed43ef718d66c8bf07d1c72d25
aitor <aitor@192.168.1.249>
2023-12-11 23:38:29 +0100

    Vista gestion de clientes terminada

--- src/controlador/ControlHabitaciones.java
+++ src/controlador/ControlHabitaciones.java
@@ -4,16 +4,23 @@
  */
 package controlador;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import javax.swing.ListModel;
 import javax.swing.event.ListSelectionEvent;
 import modelo.AccesoDAO;
 import modelo.Clientes;
+import modelo.ConexionMB;
+import modelo.clases.Habitacion;
 import vista.GestionHabitaciones;
 
 /**
  * Clase que controlará la vista GestionHabitaciones.
+ *
  * @author Cris
  */
 public class ControlHabitaciones extends ControlPadre {
@@ -24,62 +31,193 @@ public class ControlHabitaciones extends ControlPadre {
     AccesoDAO acceso;
 
     public ControlHabitaciones(GestionHabitaciones vista, AccesoDAO acceso) {
-        super(vista.getBtnCrear(), vista.getBtnBorrar(), vista.getBtnEditar(), vista.getBtnAceptar(), 
+        super(vista.getBtnCrear(), vista.getBtnBorrar(), vista.getBtnEditar(), vista.getBtnAceptar(),
                 vista.getBtnCancelar(), vista.getBtnBuscar(), vista.getList(), vista.getLblErrorCrud());
         this.vista = vista;
         this.acceso = acceso;
         clientes = new ArrayList<>();
-        
+
     }
 
     @Override
     public void valueChanged(ListSelectionEvent e) {
-         // Se activa cuando cambia el índice de lo seleccionado en la lista.
+        // Se activa cuando cambia el índice de lo seleccionado en la lista.
     }
-    
+
     @Override
     public void actionPerformed(ActionEvent e) {
         String accion = e.getActionCommand();
-        if (accion.equals("Buscar")){            
+        if (accion.equals("Buscar")) {
             buscar();
-        } else if (accion.equals("Crear")){
+        } else if (accion.equals("Crear")) {
             crear();
-        } else if (accion.equals("Borrar")){
+        } else if (accion.equals("Borrar")) {
             borrar();
-        } else if (accion.equals("Editar")){
+        } else if (accion.equals("Editar")) {
             editar();
-        } else if (accion.equals("Aceptar")){
+        } else if (accion.equals("Aceptar")) {
             aceptar();
-        }else if (accion.equals("Cancelar")){
+        } else if (accion.equals("Cancelar")) {
             cancelar();
-        } 
-    }
-    
-    
-    private void buscar(){
-        
+        }
     }
 
-    private void crear() {
-        
+    private void buscar() {
+
     }
 
-    private void borrar() {
-     
-    }
+    private void crear(Habitacion habitacion) {
+    Connection conexion = null;
+    PreparedStatement statement = null;
 
-    private void editar() {
-     
+    try {
+        // Obtener la conexión a la base de datos
+        ConexionMB conexionMB = new ConexionMB(); // Declarar e instanciar la clase ConexionMB
+        conexion = conexionMB.conectar();
+
+        // Sentencia SQL INSERT
+        String sql = "INSERT INTO habitaciones (id_hotel, capacidad, descripcion, precio_noche) VALUES (?, ?, ?, ?)";
+
+        // Crear el PreparedStatement
+        statement = conexion.prepareStatement(sql);
+
+        // Establecer los valores de los parámetros
+        statement.setInt(1, habitacion.getIdHotel());
+        statement.setInt(2, habitacion.getCapacidad());
+        statement.setString(3, habitacion.getDescripcion());
+        statement.setDouble(4, habitacion.getPrecioNoche());
+
+        // Ejecutar la sentencia SQL
+        int filasAfectadas = statement.executeUpdate();
+
+        // Verificar si al menos una fila fue afectada
+        if (filasAfectadas > 0) {
+            System.out.println("Habitación creada correctamente.");
+        } else {
+            System.out.println("No se pudo crear la habitación.");
+        }
+    } catch (SQLException e) {
+        e.printStackTrace(); // Manejo adecuado de excepciones en la práctica
+    } finally {
+        // Cerrar recursos
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conexion != null) {
+            try {
+                conexion.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
+  private void borrar(Habitacion habitacion) {
+    Connection conexion = null;
+    PreparedStatement statement = null;
+
+    try {
+        // Obtener la conexión a la base de datos
+        ConexionMB conexionMB = new ConexionMB(); // Declarar e instanciar la clase ConexionMB
+        conexion = conexionMB.conectar();
+
+        // Sentencia SQL DELETE
+        String sql = "DELETE FROM habitaciones WHERE id_habitacion = ?";
+
+        // Crear el PreparedStatement
+        statement = conexion.prepareStatement(sql);
+
+        // Establecer el valor del parámetro
+        statement.setInt(1, habitacion.getIdHotel()); // Suponiendo que id_habitacion es el identificador único
+
+        // Ejecutar la sentencia SQL
+        int filasAfectadas = statement.executeUpdate();
+
+        // Verificar si al menos una fila fue afectada
+        if (filasAfectadas > 0) {
+            System.out.println("Habitación eliminada correctamente.");
+        } else {
+            System.out.println("No se pudo eliminar la habitación.");
+        }
+    } catch (SQLException e) {
+        e.printStackTrace(); // Manejo adecuado de excepciones en la práctica
+    } finally {
+        // Cerrar recursos
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        if (conexion != null) {
+            try {
+                conexion.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+    public void editar(Habitacion habitacion) {
+        Connection conexion = null;
+        PreparedStatement statement = null;
+
+        try {
+            // Obtener la conexión a la base de datos
+            ConexionMB conexionMB = new ConexionMB(); // Declarar e instanciar la clase ConexionMB
+            conexion = conexionMB.conectar();
+
+            // Sentencia SQL UPDATE
+            String sql = "UPDATE habitaciones "
+                    + "SET capacidad = ?, descripcion = ?, pre_noch = ? "
+                    + "WHERE id_habitacion = ?";
+
+            // Crear el PreparedStatement
+            statement = conexion.prepareStatement(sql);
+
+            // Establecer los valores de los parámetros
+            statement.setInt(1, habitacion.getCapacidad());
+            statement.setString(2, habitacion.getDescripcion());
+            statement.setDouble(3, habitacion.getPrecioNoche());
+            statement.setInt(4, habitacion.getIdHotel()); // Suponiendo que id_habitacion es el identificador único
+
+            // Ejecutar la sentencia SQL
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace(); // Manejo adecuado de excepciones en la práctica
+        } finally {
+            // Cerrar recursos
+            if (statement != null) {
+                try {
+                    statement.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (conexion != null) {
+                try {
+                    conexion.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
     private void aceptar() {
-     
+
     }
 
     private void cancelar() {
-     
-    }
 
-    
+    }
 
 }
